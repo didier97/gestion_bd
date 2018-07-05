96d3b81dacc5e8f9e60bd55844ec3ec9537f190c
didier97 <yondjiodidier@yahoo.fr>
2018-07-05 16:39:06 +0200

    premier_commit

---
+++ src/main/java/com/didier/gestion_bd/GestionBdApplication.java
@@ -0,0 +1,12 @@
+package com.didier.gestion_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBdApplication.class, args);
+	}
+}
